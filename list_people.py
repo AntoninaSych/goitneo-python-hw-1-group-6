a5317119f9e1242da680f2680d821a40ad1d9237
antonina <atoninasych@gmail.com>
2023-11-30 20:57:48 +0000

    specified weekends

--- list_people.py
+++ list_people.py
@@ -1,6 +1,5 @@
 from datetime import datetime, timedelta
 from collections import defaultdict
-
 def get_birthdays_per_week(users):
     # Створюємо словник для зберігання імен іменинників за днями тижня
     birthdays_by_day = defaultdict(list)
@@ -10,6 +9,9 @@ def get_birthdays_per_week(users):
 
     # Визначаємо день початку тижня (понеділок)
     start_of_week = today - timedelta(days=today.weekday())
+
+    # Вихідні - субота і неділя
+    weekend_days = [5, 6]  # 5 - субота, 6 - неділя
 
     # Перебираємо користувачів і аналізуємо їх дати народження
     for user in users:
@@ -28,13 +30,18 @@ def get_birthdays_per_week(users):
         delta_days = (birthday_this_year - today).days
 
         # Визначаємо день тижня дня народження (переносимо на понеділок, якщо вихідний)
-        day_of_week = (start_of_week + timedelta(days=delta_days)).strftime("%A")
+        day_of_week = (start_of_week + timedelta(days=delta_days)).weekday()
+
+        # Якщо день тижня - субота або неділя, то переносимо на понеділок
+        if day_of_week in weekend_days:
+            day_of_week = 0  # Понеділок
+
+        # Переводимо числове значення дня тижня у текстовий відповідно до календаря Python
+        day_name = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"][day_of_week]
 
         # Додаємо ім'я іменинника до відповідного дня тижня
-        birthdays_by_day[day_of_week].append(name)
+        birthdays_by_day[day_name].append(name)
 
     # Виводимо результат
     for day, names in birthdays_by_day.items():
         print(f"{day}: {', '.join(names)}")
-
-
